6f766400d3b3e8d23fc723dbd4d9ccb070def7f2
Michael Uti <utimichael9@gmail.com>
2021-05-19 14:24:35 +0100

    g722: fix test

--- src/codecs/g722/g722_test.rs
+++ src/codecs/g722/g722_test.rs
@@ -1,46 +1,46 @@
 #[cfg(test)]
 mod tests {
     use crate::codecs::g722::*;
-    // #[test]
-    // fn test_g722_payload() {
-    //     let pck = G722Payloader;
+    #[test]
+    fn test_g722_payload() {
+        let pck = G722Payloader;
 
-    //     const TEST_LEN: usize = 10000;
-    //     const TEST_MTU: u16 = 1500;
+        const TEST_LEN: usize = 10000;
+        const TEST_MTU: u16 = 1500;
 
-    //     // generate random 8-bit g722 samples
-    //     let samples: Vec<u8> = (0..TEST_LEN).map(|_| rand::random::<u8>()).collect();
+        // generate random 8-bit g722 samples
+        let samples: Vec<u8> = (0..TEST_LEN).map(|_| rand::random::<u8>()).collect();
 
-    //     // make a copy, for payloader input
-    //     let samples_in = &mut [0; TEST_LEN];
-    //     samples_in.clone_from_slice(&samples);
+        // make a copy, for payloader input
+        let samples_in = &mut [0; TEST_LEN];
+        samples_in.clone_from_slice(&samples);
 
-    //     // split our samples into payloads
-    //     let payloads = pck.payload(TEST_MTU, bytes::Bytes::from_static(samples_in));
+        // split our samples into payloads
+        let payloads = pck.payload(TEST_MTU, bytes::Bytes::from_static(samples_in));
 
-    //     let outcnt = ((TEST_LEN as f64) / (TEST_MTU as f64)).ceil() as usize;
-    //     assert_eq!(
-    //         outcnt,
-    //         payloads.len(),
-    //         "Generated {} payloads instead of {}",
-    //         payloads.len(),
-    //         outcnt
-    //     );
-    //     assert_eq!(&samples, samples_in, "Modified input samples");
+        let outcnt = ((TEST_LEN as f64) / (TEST_MTU as f64)).ceil() as usize;
+        assert_eq!(
+            outcnt,
+            payloads.len(),
+            "Generated {} payloads instead of {}",
+            payloads.len(),
+            outcnt
+        );
+        assert_eq!(&samples, samples_in, "Modified input samples");
 
-    //     let samples_out = payloads.concat();
-    //     assert_eq!(&samples_out, samples_in, "Output samples don't match");
+        let samples_out = payloads.concat();
+        assert_eq!(&samples_out, samples_in, "Output samples don't match");
 
-    //     // Positive MTU, empty payload
-    //     let result = pck.payload(1, bytes::Bytes::new());
-    //     assert!(result.is_empty(), "Generated payload should be empty");
+        // Positive MTU, empty payload
+        let result = pck.payload(1, bytes::Bytes::new());
+        assert!(result.is_empty(), "Generated payload should be empty");
 
-    //     // 0 MTU, small payload
-    //     let result = pck.payload(0, bytes::Bytes::from_static(&[0x90, 0x90, 0x90]));
-    //     assert_eq!(result.len(), 0, "Generated payload should be empty");
+        // 0 MTU, small payload
+        let result = pck.payload(0, bytes::Bytes::from_static(&[0x90, 0x90, 0x90]));
+        assert_eq!(result.len(), 0, "Generated payload should be empty");
 
-    //     // Positive MTU, small payload
-    //     let result = pck.payload(10, bytes::Bytes::from_static(&[0x90, 0x90, 0x90]));
-    //     assert_eq!(result.len(), 1, "Generated payload should be the 1");
-    // }
+        // Positive MTU, small payload
+        let result = pck.payload(10, bytes::Bytes::from_static(&[0x90, 0x90, 0x90]));
+        assert_eq!(result.len(), 1, "Generated payload should be the 1");
+    }
 }
